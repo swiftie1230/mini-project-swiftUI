376599a85352d719b8158142566e80152e18231a
swiftie1230 <swiftie1230@naver.com>
2023-02-22 11:37:26 +0900

    Initial Commit

---
+++ mini_project_swiftUI/mini_project_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mini_project_swiftUIApp.swift
+//  mini_project_swiftUI
+//
+//  Created by 황서진 on 2023/02/22.
+//
+
+import SwiftUI
+
+@main
+struct mini_project_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
