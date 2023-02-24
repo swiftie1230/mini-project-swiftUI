3e1bd49c4f38a2d596b7cc6b30859c980c02af91
swiftie1230 <swiftie1230@naver.com>
2023-02-24 15:52:01 +0900

    Working with UI Controls without Badge

---
+++ mini_project_swiftUI/Model/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  mini_project_swiftUI
+//
+//  Created by 황서진 on 2023/02/23.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { rawValue }
+    }
+    
+}
